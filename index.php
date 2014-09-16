6b2d02f25090cb04051bd5678f330275027929dc
Merili <meriliasu@gmail.com>
2014-09-16 18:47:10 +0300

    If sentences
    If sentences

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Tingimuslaused</title>
+</head>
+<body>
+	<h2>Tingimuslaused</h2>
+	<?php
+	$a = 4;
+	$b = 5;
+	if ($a < $b){
+		echo "4 on väiksem kui 5";
+	}
+	$a = 8;
+	$b = 5;
+	if ($a < $b) {
+		echo "8 on väiksem kui 5";
+	}
+	?>
+</body>
+</html>
+	
